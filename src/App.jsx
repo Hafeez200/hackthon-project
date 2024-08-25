2812b8a729976d428bf47b92035cc921ed7e1c4d
Hafeez200 <saibhaji24@email.com>
2024-08-25 13:56:14 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div className="welcome-page">
+      <h1>Welcome to Your React-Vite App!</h1>
+      <p>This is the landing page of your application.</p>
+      {/* Add more content or components here */}
+    </div>
+  );
+}
+
+export default Welcome;
